8bf0899bda3d5d4f2b788d2539d1dfca0633631e
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-05-06 22:12:23 +0300

    changes to the file str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,19 @@
 def all_capital_letters(text):
+    """
+    функция  возвращает строку со всеми заглавными буквами
+    :param text: str
+    :retнедавнийurn: str
+    """
     new_text_1 = text
     return new_text_1.upper()
+
+
+def first_capital_letters(text):
+    """
+    функция делает заглавными первые буквы каждого слова в строке,
+    поступившей на вход функции
+    :param text: str
+    :return: str
+    """
+    new_text_2 = text
+    return new_text_2.title()
